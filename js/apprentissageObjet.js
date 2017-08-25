b0188d0408ceffacb5b352817752d6f444b503a2
geasseur <baptiste.ventura@neuf.fr>
2017-08-25 14:08:40 +0200

    objet rangement toujours en cours, tableau réponse remplit mais problème de répétition

--- js/apprentissageObjet.js
+++ js/apprentissageObjet.js
@@ -23,7 +23,7 @@ function chargementTableau(){
   xmlhttp.send();
 }
 
-//objet sélectionnant quel signe proposer a l'utilisateur, prenant aussi la bonne réponse et 3 réponses aléatoire placé dans un tableau
+//objet sélectionnant quel signe proposer a l'utilisateur, prenant aussi la bonne réponse
 function selection(signe){
   this.signe = signe;
   this.selectionSigneJap = function (signe){
@@ -43,7 +43,7 @@ function selection(signe){
   console.log(signe);
 }
 
-//objet rangeant dans l'interface l'image et les réponses proposé à l'utilisateur
+//objet rangeant dans l'interface l'image et les réponses proposé à l'utilisateur et 3 réponses aléatoire placé dans un tableau
 function rangementSigne(signeQuestion, choixReponse){
   this.signeQuestion = signeQuestion;
   this.choixReponse = choixReponse;
@@ -51,15 +51,27 @@ function rangementSigne(signeQuestion, choixReponse){
   this.dispositionSigneJap = function(signeQuestion){
     console.log(signeQuestion);
     $("#imgQuestion").attr('src', signeQuestion);
-    /*var positionReponse = Math.floor(Math.random()*(4 - 1)+1);
-    tabReponses[positionReponse] = choixReponse;
-    console.log(tabReponses);*/
   }
   this.dispositionLettre = function(choixReponse){
-    var positionReponse = Math.floor(Math.random()*(4 - 1)+1);
+    var positionReponse = Math.floor(Math.random()*(5 - 1));
+    console.log(positionReponse);
     tabReponses[positionReponse] = choixReponse;
     console.log(tabReponses);
     console.log(signe);
+    for (var i = 0; i < 3; i++) {
+      positionReponse = Math.floor(Math.random()*(4 - 1));
+      var reponseAleatoire = Math.floor(Math.random()*(tabReponses.length-1)+1);
+      if (tabReponses[positionReponse] == "vide" ) {
+        tabReponses[positionReponse] = signe.japon[reponseAleatoire]["latin"];
+      }
+      else if (tabReponses[positionReponse] == choixReponse){
+        return;
+      }
+      else{
+        i--;
+      }
+    }
+    console.log(tabReponses);
   }
 }
 
